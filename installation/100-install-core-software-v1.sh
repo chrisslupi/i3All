cc6269a62376d1ebb7a3e739e469c14007049ae8
chrisslupi <gudmei@web.de>
2018-02-08 13:16:55 +0100

    Comment : first commit on Thu Feb  8 13:16:55 CET 2018

---
+++ installation/100-install-core-software-v1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+
+##################################################################################################################
+
+#software from 'normal' repositories
+sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
+sudo pacman -S --noconfirm --needed darktable dconf-editor
+sudo pacman -S --noconfirm --needed dmidecode
+sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
+sudo pacman -S --noconfirm --needed galculator gimp git gksu glances gnome-disk-utility
+# sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool
+sudo pacman -S --noconfirm --needed gparted gpick grsync
+sudo pacman -S --noconfirm --needed hardinfo hddtemp htop
+sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors lsb-release meld mlocate mpv
+sudo pacman -S --noconfirm --needed nemo net-tools notify-osd openshot plank polkit-gnome
+sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell
+sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat
+sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk tumbler
+sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter
+sudo pacman -S --noconfirm --needed r gcc-fortran libreoffice-fresh atom vim hunspell-en hunspell-de
+sudo pacman -S --noconfirm --needed texlive-lang kile texmaker
+sudo pacman -S --noconfirm --needed nextcloud
+
+sudo systemctl enable vnstat
+sudo systemctl start vnstat
+
+###############################################################################################
+
+# installation of zippers and unzippers
+sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils  uudeview  arj cabextract file-roller
+
+###############################################################################################
+
+
+echo "################################################################"
+echo "###################    core software installed  ################"
+echo "################################################################"
---
+++ installation/690-install-personal-settings-i3-configuration-v1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+
+##################################################################################################################
+
+
+git clone https://github.com/erikdubois/archi3 ~/.config/i3
+
+echo "################################################################"
+echo "#########       personal settings installed     ################"
+echo "################################################################"
+
+echo "You can reboot and start computing"
+echo
+echo
+echo "Do not run script 700 yet. You need to start firefox before you do."
+echo
+echo "Type in the terminal the following code"
+echo "sudo reboot"
